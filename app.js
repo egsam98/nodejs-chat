2989b6d38ad967b62731acb76940b65ff734ed83
Egor <egsam98@gmail.com>
2019-01-17 06:15:58 +0300

    v1.1.9

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ const server = http.createServer(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'public', 'index.html'));
-    });
-}
+// const path = require('path');
+// app.use(express.static(path.join(__dirname, 'public')));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'))
+//
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../client', 'public', 'index.html'));
+//     });
+// }
 
 let clients = [];
 clients.withoutSocket = function() {
